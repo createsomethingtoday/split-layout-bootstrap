9a8a56df23851dea07b5ed6910b7471705a06557
MicahRyan <Micah.Johnson@maritz.com>
2016-04-05 10:30:16 -0500

    Update Hover Options
    Change hover/animation to buttons of landing page.

--- SplitLayout/js/hover.js
+++ SplitLayout/js/hover.js
@@ -1,16 +1,16 @@
-$('.side-right').hover(function(){
+$('.side-right-hover').hover(function(){
     $('.side-right').animate({width: '100%'}, 500)
 }, function(){
     $('.side-right').animate({width: '45%'}, 500)
 });
 
-$('.side-left').hover(function(){
+$('.side-left-hover').hover(function(){
     $('.side-left').animate({width: '100%'}, 500)
 }, function(){
     $('.side-left').animate({width: '55%'}, 500)
 });
 
-$('.side-left').hover(function(){
+$('.side-left-hover').hover(function(){
     $('.side-right').animate({'z-index': '1'}, 1)
 }, function(){
     $('.side-right').animate({'z-index': '101'}, 500)
