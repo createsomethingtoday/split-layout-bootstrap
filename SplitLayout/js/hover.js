bd84a9f35504a21df77028a75ee58001ef2e7869
MicahRyan <Micah.Johnson@maritz.com>
2016-04-20 16:46:06 -0500

    Change Image Div Code
    Moved background images to different divs, and only animate the left
    image that is anchored to the browser window.

--- SplitLayout/js/hover.js
+++ SplitLayout/js/hover.js
@@ -1,17 +1,17 @@
 $('.side-right-hover').hover(function(){
-    $('.side-right').animate({width: '100%'}, 500)
+    $('.left-image').animate({width: '2%'}, 400)
 }, function(){
-    $('.side-right').animate({width: '45%'}, 500)
+    $('.left-image').animate({width: '55%'}, 400)
 });
 
 $('.side-left-hover').hover(function(){
-    $('.side-left').animate({width: '100%'}, 500)
+    $('.left-image').animate({width: '98%'}, 400)
 }, function(){
-    $('.side-left').animate({width: '55%'}, 500)
+    $('.left-image').animate({width: '55%'}, 400)
 });
 
-$('.side-left-hover').hover(function(){
+$('.side-right-hover').hover(function(){
     $('.side-right').animate({'z-index': '1'}, 1)
 }, function(){
-    $('.side-right').animate({'z-index': '101'}, 500)
+    $('.side-right').animate({'z-index': '101'}, 600)
 });
---
+++ SplitLayout/js/slide.js
@@ -0,0 +1,3 @@
+$('.side-right-hover').hover(function(){
+    $('.side-right').addClass('slide-hover'); 
+});
