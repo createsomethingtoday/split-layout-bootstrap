9ebca39c1be07f65694d292a45438ef44faeeec1
MicahRyan <contact@micahryan.com>
2016-08-06 22:59:52 -0500

    Footer Javascript

---
+++ SplitLayout/js/footer.js
@@ -0,0 +1,15 @@
+$("#button-right").click(function() {
+    $("#site-footer").fadeOut('fast');
+});
+
+$("#button-left").click(function() {
+    $("#site-footer").fadeOut('fast');
+});
+
+$("#button-back-right").click(function() {
+    $("#site-footer").fadeIn('fast');
+});
+
+$("#button-back-left").click(function() {
+    $("#site-footer").fadeIn('fast');
+});
